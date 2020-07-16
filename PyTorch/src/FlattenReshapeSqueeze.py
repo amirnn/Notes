0602db30a3105f0d540f9467add6470592ab90a8
Amir Nourinia <amir.nourinia@gmail.com>
2020-07-16 12:06:49 +0200

    PT update.

--- PyTorch/src/FlattenReshapeSqueeze.py
+++ PyTorch/src/FlattenReshapeSqueeze.py
@@ -22,4 +22,44 @@ t = torch.tensor(
 print(t.size())
 print(t.shape)
 
+# Rank of a Tensor. The Rank represesnts the dimensionality of the Tensor.
 print("This is the rank of t: {}".format(len(t.shape)))
+
+# Scalar components of a Tensor i.e. Number of elements
+torch.tensor(t.shape).prod()
+# Same as before
+t.numel()
+
+print(torch.tensor(t.shape).prod()," is same as: " , t.numel())
+
+# reshape, not that the axis sizes are factors of 12
+t.reshape(1,12)
+t.reshape(2,6)
+t.reshape(3,4)
+print(t.reshape(1,12))
+print(t.reshape(2,6))
+print(t.reshape(3,4))
+
+# To get rid of the un-needed extra dimentionality i.e. get rid of axis that are 1
+# we use squeeze() method.
+
+print("Squeezed tensor: " , t.reshape(1,12).squeeze())
+
+# This shows that all these operations are only views not copies.
+# since, the original data is intact.
+print("The original tensor after squeeze and reshape: {}".format(t))
+
+# To add back the axis we got rid of previously, we use unsqueeze(dim=n) call
+# This adds an axis with length 1 at the dim=n, but note that n can only be rank + 1
+print("After unsqueeze dim=0: \n",t.reshape(1,12).squeeze().unsqueeze(dim=0))
+print("After unsqueeze dim=1: \n",t.reshape(1,12).squeeze().unsqueeze(dim=1))
+# Flattening a Tensor
+# We flatten the image to 1D array or a rank 1 tensor.
+
+def flatten(t):
+    return t.reshape(1,-1).squeeze()
+
+print("Our own flattened function: {}".format(flatten(t)))
+
+
+# To concatinate tensors we use torch.cat
