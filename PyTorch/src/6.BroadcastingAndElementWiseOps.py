de368449dc907011453eb11bf435710940f6a045
Amir Nourinia <amir.nourinia@gmail.com>
2020-07-17 14:01:49 +0200

    PT Updated.

--- PyTorch/src/6.BroadcastingAndElementWiseOps.py
+++ PyTorch/src/6.BroadcastingAndElementWiseOps.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import torch
 
+# Element-wise , componenet-wise, point-wise
+
 # If the two tensors have the same shape, we can perform element wise
 # operations on them. +-*/ are all element wise operations.
 
@@ -20,11 +22,18 @@ filterMatrix = result > 0
 print("Filter Matrix is:\n",filterMatrix)
 # Will print the values that were bigger than 1.
 print("Values bigger than 1:\n",result[filterMatrix])
-print("Values bigger t")
+# For element wise multiplication use *. For cassting use result.int(), or double() etc.
+print("Values bigger than 1 in their place as a matrix.\n",
+result * filterMatrix.int()
+)
 
-# We can get 0 or 1 instead of True or False following the
+# We can get the same results using the in-built functions
+# Greater equals
+print("Greater equals to 0:\n",result ,"\n", result.ge(0))
+# Greater
+print("Greater than 0:\n",result.gt(0))
 
 # Becasue of Broadcasting we can do the following operation.
 t3 = torch.rand((5,1))
 
-print(t3 + t1)
+print("Broadcasting works:\n", t3 + t1)
