d1eb38b334348fe5d1f723c01cc69d1ba80a4dc1
Amir Nourinia <amir.nourinia@gmail.com>
2020-07-17 18:43:20 +0200

    PT updated, ETL added.

--- PyTorch/src/11.CNNImagePreparation.py
+++ PyTorch/src/11.CNNImagePreparation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # 1. prepare the data
-# ETL Extraxt, Transform, Load
+# ETL: Extract, Transform, Load
 
 # 2. build the model
 
@@ -14,31 +14,31 @@ import torchvision
 import torchvision.transforms as transforms
 
 # An abstract class for representing a dataset
-import torch.utils.data.dataset
+from torch.utils.data import Dataset
 
-# Wraos a dataset and provides access to the underlying data.
-import torch.utils.data.dataloader
+# Wraps a dataset and provides access to the underlying data.
+from torch.utils.data import DataLoader
 
 #  An example implmentation of a Dataset abstract class.
 # Two methods are necessary, __getitem__ and __len__
-# class OHLC(Dataset):
-#     def __init__(self, csv_file):
-#         super().__init__()
-#         self.data = pd.read_csv(csv_file)
-#     def __getitem__(self, index):
-#         r = self.data.iloc[index]
-#         label = torch.tensor(r.is_up_day, dtype=torch.long)
-#         sample = self.normalize(torch.tensor([
-#             r.open, r.high, r.low, t.close
-#         ]))
-#         return sample, label
-#     def __len__(self):
-#         return len(self.data)
+class OHLC(Dataset):
+    def __init__(self, csv_file):
+        super().__init__()
+        self.data = pd.read_csv(csv_file)
+    def __getitem__(self, index):
+        r = self.data.iloc[index]
+        label = torch.tensor(r.is_up_day, dtype=torch.long)
+        sample = self.normalize(torch.tensor([
+            r.open, r.high, r.low, t.close
+        ]))
+        return sample, label
+    def __len__(self):
+        return len(self.data)
 
 # torchvision provides us with an implmentation of the Dataset class for
 # the mnist and fashionmnist databases.
 # torchvision > datasets > mnist.py
-
+# FashionMNIST dataset has 60,000 training data and 10,000 in test data.
 trainFASHIONSet = torchvision.datasets.FashionMNIST(
     root="./data/FashionMNIST",
     train = True,
@@ -47,7 +47,16 @@ trainFASHIONSet = torchvision.datasets.FashionMNIST(
         transforms.ToTensor()
     ])
 )
+testFASHIONSet = torchvision.datasets.FashionMNIST(
+    root="./data/FashionMNIST",
+    train=False,
+    download=False,
+    transform=transforms.Compose([
+        transforms.ToTensor()
+    ])
+)
 
+# The MNIST dataset 
 trainMNIST = torchvision.datasets.MNIST(
     root="./data/MNIST",
     train=True,
@@ -56,3 +65,19 @@ trainMNIST = torchvision.datasets.MNIST(
         transforms.ToTensor()
     ])
 )
+testMNIST = torchvision.datasets.MNIST(
+    root="./data/MNIST",
+    train=False,
+    download=False,
+    transform=transforms.Compose([
+        transforms.ToTensor()
+    ])
+)
+
+# Now we wrap the Dataset objects with a DataLoader.
+# Fashion Data Loaders
+trainLoaderFashion = torch.utils.data.DataLoader(trainFASHIONSet)
+testLoaderFashion = torch.utils.data.DataLoader(testFASHIONSet)
+# MNIST Data Loaders
+trainLoaderMNIST = torch.utils.data.DataLoader(trainMNIST)
+testLoaderMNIST = torch.utils.data.DataLoader(testMNIST)
